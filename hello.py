dad5faba33df0cb59b99d0264bb786f76690fb1a
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-02-04 19:16:45 +0900

    ruff適用

--- hello.py
+++ hello.py
@@ -4,5 +4,6 @@ from b.sub import f_sub2
 def main():
     print(f_sub2())
 
+
 if __name__ == "__main__":
     main()
--- tests/b/test_sub.py
+++ tests/b/test_sub.py
@@ -3,4 +3,3 @@ from b.sub import f_sub2
 
 def test_f_sub2():
     assert f_sub2() == "b/f_sub2|a/f_sub"
-
